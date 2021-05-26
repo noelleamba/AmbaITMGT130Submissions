8b9af63fae3dcad8b378a53d3ea1031b677e3432
noelleamba <84843438+noelleamba@users.noreply.github.com>
2021-05-26 18:13:04 +0800

    Add files via upload

---
+++ Amba-Noelle-introtopython.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #### 170094
+# #### Amba 
+# #### 4 BS COMTECH
+
+# In[33]:
+
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+z = int(input("Enter third number: "))
+
+print("The average is  " + str(int(x+y+z)/3))
+
+
+# In[ ]:
+
+
+
+
